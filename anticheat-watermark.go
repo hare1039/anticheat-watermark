f753c5c1d4182651a8e260b0cbcd6f2966b28aa3
hare1039 <hare39393@gmail.com>
2020-08-18 19:22:58 -0400

    Saperate as library

--- anticheat-watermark.go
+++ anticheat-watermark.go
@@ -1,15 +1,10 @@
-package main
+package watermark
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"path/filepath"
-	"strings"
 	"sync"
 
-	//	unicommon "github.com/unidoc/unidoc/common"
-	quote "github.com/kballard/go-shellquote"
 	"github.com/unidoc/unidoc/pdf/creator"
 	"github.com/unidoc/unidoc/pdf/model"
 	"github.com/unidoc/unidoc/pdf/model/fonts"
@@ -86,7 +81,7 @@ func drawOnePage(c *creator.Creator, word string) {
 	}
 }
 
-func drawPDF(wg *sync.WaitGroup, pdffile string, word string, output string) {
+func DrawPDF(wg *sync.WaitGroup, pdffile string, word string, output string) {
 	fmt.Println("Starting on: ", word)
 	defer wg.Done()
 
@@ -126,65 +121,9 @@ func drawPDF(wg *sync.WaitGroup, pdffile string, word string, output string) {
 	fmt.Println(word, " finished")
 }
 
-func main() {
-	var pdffile, namefile string
-	if len(os.Args) != 3 {
-		scanner := bufio.NewScanner(os.Stdin)
-		fmt.Print("Please enter the pdf file: ")
-		scanner.Scan()
-
-		pdflist, err := quote.Split(strings.TrimSpace(scanner.Text()))
-		if err != nil {
-			panic(err)
-		}
-		pdffile = pdflist[0]
-
-		fmt.Print("Please enter the name file: ")
-		scanner.Scan()
-		namefilelist, err := quote.Split(strings.TrimSpace(scanner.Text()))
-		if err != nil {
-			panic(err)
-		}
-		namefile = namefilelist[0]
-	} else {
-		pdffile = os.Args[1]
-		namefile = os.Args[2]
-	}
-
-	if strings.Contains(pdffile, "pdf") {
-	} else if strings.Contains(namefile, "pdf") {
-		pdffile, namefile = namefile, pdffile
-	} else {
-		fmt.Println("Can't find 'pdf' in argv")
-		fmt.Scanln()
-		return
-	}
-
-	fp, err := os.Open(namefile)
-	if err != nil {
-		fmt.Println(err, "Abort.")
-		fmt.Scanln()
-		panic(err)
-	}
-	defer fp.Close()
-
-	fullName := strings.Split(filepath.Base(pdffile), ".")[0]
-	os.Chdir(filepath.Dir(pdffile))
-
-	err = os.Mkdir(fullName, 0755)
-	if err != nil {
-		fmt.Println("ERROR: folder ", fullName, " exist. Abort.")
-		fmt.Scanln()
-		panic(err)
-	}
-	scanner := bufio.NewScanner(fp)
-
-	fmt.Println("Start adding anti-cheat watermark")
+func DrawPDFSingle(pdffile string, word string, output string) {
 	var wg sync.WaitGroup
-	for scanner.Scan() {
-		name := scanner.Text()
-		wg.Add(1)
-		go drawPDF(&wg, pdffile, name, fullName+"/"+name+".pdf")
-	}
+	wg.Add(1)
+	go DrawPDF(&wg, pdffile, word, output)
 	wg.Wait()
 }
---
+++ cmd/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	watermark "github.com/hare1039/anticheat-watermark"
+	quote "github.com/kballard/go-shellquote"
+)
+
+func main() {
+	var pdffile, namefile string
+	if len(os.Args) != 3 {
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Print("Please enter the pdf file: ")
+		scanner.Scan()
+
+		pdflist, err := quote.Split(strings.TrimSpace(scanner.Text()))
+		if err != nil {
+			panic(err)
+		}
+		pdffile = pdflist[0]
+
+		fmt.Print("Please enter the name file: ")
+		scanner.Scan()
+		namefilelist, err := quote.Split(strings.TrimSpace(scanner.Text()))
+		if err != nil {
+			panic(err)
+		}
+		namefile = namefilelist[0]
+	} else {
+		pdffile = os.Args[1]
+		namefile = os.Args[2]
+	}
+
+	if strings.Contains(pdffile, "pdf") {
+	} else if strings.Contains(namefile, "pdf") {
+		pdffile, namefile = namefile, pdffile
+	} else {
+		fmt.Println("Can't find 'pdf' in argv")
+		fmt.Scanln()
+		return
+	}
+
+	fp, err := os.Open(namefile)
+	if err != nil {
+		fmt.Println(err, "Abort.")
+		fmt.Scanln()
+		panic(err)
+	}
+	defer fp.Close()
+
+	fullName := strings.Split(filepath.Base(pdffile), ".")[0]
+	os.Chdir(filepath.Dir(pdffile))
+
+	err = os.Mkdir(fullName, 0755)
+	if err != nil {
+		fmt.Println("ERROR: folder ", fullName, " exist. Abort.")
+		fmt.Scanln()
+		panic(err)
+	}
+	scanner := bufio.NewScanner(fp)
+
+	fmt.Println("Start adding anti-cheat watermark")
+	var wg sync.WaitGroup
+	for scanner.Scan() {
+		name := scanner.Text()
+		wg.Add(1)
+		go watermark.DrawPDF(&wg, pdffile, name, fullName+"/"+name+".pdf")
+	}
+	wg.Wait()
+}
